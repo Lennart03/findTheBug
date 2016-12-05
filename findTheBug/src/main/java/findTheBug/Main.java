839be897265655a9646f7502b1158557e718e1b0
Lennart Van Damme <Lennart.VanDamme@realdolmen.com>
2016-12-05 15:13:00 +0100

    Update Main.java

--- findTheBug/src/main/java/findTheBug/Main.java
+++ findTheBug/src/main/java/findTheBug/Main.java
@@ -4,12 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		final int [] INDEX_ARRAY = {0,1,2,3,4};
-		int [] values = {0,1,2,3,4};
-		
-		for(int i = 0; i < INDEX_ARRAY.length; i++){
-			values[INDEX_ARRAY[i]] = i;
-		}
+		System.out.println("HELLO");
 
 	}
 
